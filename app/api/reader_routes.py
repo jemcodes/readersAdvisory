9f78adadc1a3c1528aedebdf10533e2876be3f0e
jane martin <martin.jane.ellis@gmail.com>
2021-06-03 14:06:20 -0400

    Add update and delete routes for reader preferences

--- app/api/reader_routes.py
+++ app/api/reader_routes.py
@@ -26,9 +26,7 @@ def add_reader_preferences():
             cover_choices=cover_choices,
             genre_choices=genre_choices,
             author_choices=author_choices,
-            other_choices=other_choices,
-            reader_id=reader_id
-        )
+            other_choices=other_choices)
         db.session.add(new_reader_preferences)
         db.session.commit()
         return new_reader_preferences.to_dict()
@@ -46,7 +44,7 @@ def reader(reader_id):
 @reader_routes.route('/<int:reader_id>/preferences', methods=['GET'])
 @login_required
 def get_reader_preferences(reader_id):
-    """Get single reader's preferences'"""
+    """Get single reader's preferences"""
     preferences = ReaderPreference.query.get(reader_id)
     return preferences.to_dict()
 
@@ -54,14 +52,27 @@ def get_reader_preferences(reader_id):
 @reader_routes.route('/<int:reader_id>/preferences', methods=['PUT'])
 @login_required
 def edit_reader_preferences(reader_id):
+    """Update single reader's preferences"""
     form = ReaderPreferenceForm()
     form['csrf_token'].data = request.cookies['csrf_token']
     if form.validate_on_submit():
         updated_preferences = ReaderPreference.query.get(reader_id)
-        updated_preferences.user_name = form.data['rating']
-        updated_preferences.review = form.data['review']
+        updated_preferences.user_name = form.data['user_name']
+        updated_preferences.cover_choices = form.data['cover_choices']
+        updated_preferences.genre_choices = form.data['genre_choices']
+        updated_preferences.author_choices = form.data['author_choices']
+        updated_preferences.other_choices = form.data['other_choices']
         db.session.add(updated_preferences)
         db.session.commit()
         return updated_preferences.to_dict()
     return {'errors': validation_errors_to_error_messages(form.errors)}, 401
 
+
+@reader_routes.route('/<int:reader_id>/preferences', methods=['DELETE'])
+@login_required
+def delete_reader_preferences(reader_id):
+    """Delete single reader's preferences"""
+    preferences_to_delete = ReaderPreference.query.get(reader_id)
+    db.session.delete(preferences_to_delete)
+    db.session.commit()
+    return "all good!"

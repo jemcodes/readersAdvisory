eb67cf9238c2cd962397007ca37eefdf66adddc2
jane martin <martin.jane.ellis@gmail.com>
2021-06-06 18:22:51 -0400

    Add basic styling to preferences view

--- react-app/src/components/ShowPreferences.js
+++ react-app/src/components/ShowPreferences.js
@@ -17,14 +17,24 @@ const ShowPreferences = () => {
     return (
        <div id="preferences-container">
            <div id="preferences-contents">
-            <h2>Preferences</h2>
+            <h3 id="preferences-header">Reader Preferences</h3>
                 {preferences && (
                     <ul>
-                        <li>Username: {preferences.user_name}</li>
-                        <li>Cover Preference: {preferences.cover_choices}</li>
-                        <li>Genres: {preferences.genre_choices.split(', ').map(choice => (<p>{choice}</p>))}</li>
-                        <li>Authors: {preferences.author_choices.split(', ').map(choice => (<p>{choice}</p>))}</li>
-                        <li>Notes: {preferences.other_choices}</li>
+                        <div className="preference-list-div">
+                            <li className="preference-list-items">Username: <p className="preference-list-contents">{preferences.user_name}</p></li>
+                        </div>
+                        <div className="preference-list-div">
+                            <li className="preference-list-items">Cover Preference: <p className="preference-list-contents">{preferences.cover_choices}</p></li>
+                        </div>
+                        <div>
+                            <li className="preference-list-items">Genres: {preferences.genre_choices.split(', ').map(choice => (<p className="preference-list-contents">{choice}</p>))}</li>
+                        </div>
+                        <div>
+                            <li className="preference-list-items">Authors: {preferences.author_choices.split(', ').map(choice => (<p className="preference-list-contents">{choice}</p>))}</li>
+                        </div>
+                        <div>
+                            <li className="preference-list-items">Notes: <p className="preference-list-contents">{preferences.other_choices}</p></li>
+                        </div>
                     </ul>
                 )}
             </div>

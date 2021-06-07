e8906b8393f71b6756bdc4b95591d8afc317456a
jane martin <martin.jane.ellis@gmail.com>
2021-06-07 10:26:57 -0400

    Add changes to subscription store

--- react-app/src/store/subscription.js
+++ react-app/src/store/subscription.js
@@ -90,8 +90,8 @@ export default function subscription(state = initialState, action) {
     switch (action.type) {
         case GET_SUBSCRIPTION:
             nextState = {
-                subscription_type: action.payload.subscription,
-                payment_method: action.payload.payment,
+                subscription_type: action.payload.subscription_type,
+                payment_method: action.payload.payment_method,
                 reader_id: action.payload.reader_id
             }
 

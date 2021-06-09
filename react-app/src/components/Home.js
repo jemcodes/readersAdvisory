42d6493006bd68037e603b9d54082586e3b06e6b
jane martin <martin.jane.ellis@gmail.com>
2021-06-09 11:47:49 -0400

    Fix spacing issue between nav bar and menu

--- react-app/src/components/Home.js
+++ react-app/src/components/Home.js
@@ -1,11 +1,17 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import './styles/home.css'
 
 
 const Home = () => {
     return (
-        <div id="home-page-greeting">
-            <h1>Discover a new reading <br></br>experience</h1>
+        <div>
+            <div id="home-page-greeting">
+                <h1>Discover a new reading <br></br>experience</h1>
+            </div>
+            <NavLink className="nav-links" to="/sign-up" exact={true} activeClassName="active">
+                <button className="home-page-button">Get Started</button>
+            </NavLink>
         </div>
     )
 }

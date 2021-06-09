8587673889802faa036518dda3da6b409ec5b0af
jane martin <martin.jane.ellis@gmail.com>
2021-06-09 16:47:53 -0400

    Add to product store and ability to show products

--- react-app/src/components/ShowProducts.js
+++ react-app/src/components/ShowProducts.js
@@ -1,23 +1,22 @@
 import React, { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux"
 import { useParams, NavLink } from 'react-router-dom';
-import { showProduct } from '../store/product';
+import { showAllProducts } from '../store/product';
 
 const ShowProducts = () => {
     const dispatch = useDispatch();
     const reader = useSelector(state => state.session.reader);
-    const products = useSelector(state => state.product)
+    const product = useSelector(state => state.product)
     let authors;
 
     useEffect(() => {
-        dispatch(showProducts())
+        dispatch(showAllProducts())
     }, [dispatch])
 
     return (
         <div id="product-container">
             <div id="product-contents">
-                <h2 id="product-header">product</h2>
-                {product.product_type ? (
+                <h2 id="product-header">Products</h2>
                     <div className="product-list-div">
                         <ul>
                             <li className="product-list-items">Title: {product.title}</li>
@@ -25,25 +24,7 @@ const ShowProducts = () => {
                             <li className="product-list-items">Genres: {product.genres}</li>
                             <li className="product-list-items">Author: {product.author}</li>
                         </ul>
-                        <NavLink to={`/readers/${reader_id}/product/update`}>
-                            <button id="update-product-btn" type="button">
-                                Add to order
-                    </button>
-                        </NavLink>
                     </div>
-                ) : (
-                    <div className="product-list-div">
-                        <h1>Start a new product!</h1>
-                        <NavLink to={`/readers/${reader_id}/product/new`}>
-                            <button id="start-product-btn" type="button">
-                                Start New product
-                        </button>
-                        </NavLink>
-                    </div>
-                )}
-                <div>
-                    <Footer />
-                </div>
             </div>
         </div>
     );

705b2c78bfc09060a311ff88638efb572f2a2da2
jane martin <martin.jane.ellis@gmail.com>
2021-06-08 23:34:38 -0400

    Dual login working with session

--- react-app/src/components/auth/ProtectedRoute.js
+++ react-app/src/components/auth/ProtectedRoute.js
@@ -4,10 +4,12 @@ import { useSelector } from "react-redux";
 
 const ProtectedRoute = props => {
   const reader = useSelector(state => state.session.reader)
+  const advisor = useSelector(state => state.session.advisor)
   
+  // TODO: redirect to advisor login for advisor routes
   return (
     <Route {...props}>
-      {(reader) ? props.children  : <Redirect to="/login" />}
+      {(reader || advisor) ? props.children  : <Redirect to="/login" />}
     </Route>
   )
 };

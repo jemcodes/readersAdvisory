64f9be91e2578f104196fe718dc8adecde76a9f8
jane martin <martin.jane.ellis@gmail.com>
2021-06-10 10:50:56 -0400

    Begin styling for reader nav menu

--- react-app/src/components/NavBar.js
+++ react-app/src/components/NavBar.js
@@ -44,8 +44,43 @@ const NavBar = () => {
         </div>
       </nav>
         {showReaderMenu && ( 
-            <div className="menu-container">
-            <div className="nav-links-right">
+            <div className="reader-menu-container">
+            <div className="reader-nav-links">
+              <h1 id="author-heading">A Few Of Our Authors</h1>
+              <div id="author-container">
+                  <div id="left-authors">
+                      <a target="_blank" rel="noreferrer" href="https://akrscott.com/">A.K.R. Scott</a>
+                      <br></br>
+                      <a target="_blank" rel="noreferrer" href="https://tamaralush.com/">Tamara Lush</a>
+                      <br></br>
+                      <a target="_blank" rel="noreferrer" href="https://www.joannemachin.com/">Joanne Machin</a>
+                      <br></br>
+                      <a target="_blank" rel="noreferrer" href="https://autumnjoneslake.com/">Autumn Jones Lake</a>
+                  </div>
+                  <div id="right-authors">
+                    <a target="_blank" rel="noreferrer" href="https://www.dannikadark.net/">Dannika Dark</a>
+                    <br></br>
+                    <a target="_blank" rel="noreferrer" href="https://www.aljacksonauthor.com/">A. L. Jackson</a>
+                    <br></br>
+                    <a target="_blank" rel="noreferrer" href="https://www.cariancolewrites.com/">Carian Cole</a>
+                    <br></br>
+                    <a target="_blank" rel="noreferrer" href="https://jemartinbooks.com/">J. E. Martin</a>
+                  </div>
+              <div className="menu-divider"></div>
+              </div>
+            </div>
+          <div className="about-nav-links">
+            <h1 id="about-heading">About the Developer</h1>
+            <div id="about-container">
+              <div id="left-about">
+                <a target="_blank" rel="noreferrer" href="https://github.com/jemcodes/">GitHub</a>
+              </div>
+              <div id="right-about">
+                <a target="_blank" rel="noreferrer" href="https://www.linkedin.com/in/jemcodes/">LinkedIn</a>
+              </div>
+              <div className="menu-divider"></div>
+            </div>
+          </div>
             <NavLink className="nav-links" to="/sign-up" onClick={toggleReaderMenu} exact={true} activeClassName="active">
               <button className="nav-bar-buttons">Get Started</button>
             </NavLink>
@@ -53,8 +88,7 @@ const NavBar = () => {
                   <button className="nav-bar-buttons">Sign In</button>
                 </NavLink>
                 <DemoUser />
-              </div>  
-      </div>
+              </div>
         )}
       {showAdvisorMenu && (
         <div className="menu-container">
